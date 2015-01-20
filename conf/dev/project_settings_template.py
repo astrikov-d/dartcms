dc494acfbbf37de16d067864bad45114b548f6a6
Dmitry Astrikov <astrikov.d@gmail.com>
2015-01-20 17:57:28 +0700

    Add project settings template

---
+++ conf/dev/project_settings_template.py
@@ -0,0 +1,28 @@
+__author__ = 'Dmitry Astrikov'
+
+# Project and environment specific settings.
+
+PROJECT_ROOT = '/var/www/dartcms'
+
+APP_URL = "http://dartcms.dev:8000"
+
+PORT = 8000
+
+DEBUG = True
+
+DEFAULT_FROM_EMAIL = 'no-reply@dartcms.dev'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+
+YM_COUNTER_ID = None
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dartcms',
+        'USER': 'dartcms',
+        'PASSWORD': '********',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
