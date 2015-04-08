23081ac73975d300d3c1259124cc4df98483f812
slava <master@neucom.ru>
2015-04-08 15:49:00 +0700

    Small changes in InsertObjectView

--- lib/views/adm/generic.py
+++ lib/views/adm/generic.py
@@ -293,7 +293,8 @@ class InsertObjectView(AdminMixin, CreateView):
                 fk = "%s_id" % self.parent_model.__name__.lower()
 
             setattr(obj, fk, self.kwargs[self.parent_kwarg_name])
-            obj.save()
+            self.object = obj.save()
+            form.save_m2m()
             return HttpResponseRedirect(self.get_success_url())
         else:
             return super(InsertObjectView, self).form_valid(form)
