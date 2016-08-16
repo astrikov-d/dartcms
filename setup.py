c108137d73896608a2c9139bea95f33fb8ba2165
slava <master@neucom.ru>
2016-08-16 10:20:12 +0700

    Fix pages

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ packages = find_packages()
 
 setup(
     name='dartcms',
-    version='0.0.25',
+    version='0.0.26',
     description='DartCMS - open-source content management system for Django',
     long_description='DartCMS is an open-source content management system based on the popular '
                      'Django Framework. It is friendly for developers and end-users.',
