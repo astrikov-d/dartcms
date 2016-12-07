1031b27fc8b2c223eeb945ffbbf4f9f0ad8e1834
Dmitry Astrikov <astrikov.d@gmail.com>
2016-12-07 09:29:33 +0700

    Fix js

--- dartcms/static/dartcms/js/apps/base/http.js
+++ dartcms/static/dartcms/js/apps/base/http.js
@@ -3,5 +3,5 @@ var request = {
 };
 
 decodeURI(location.search).substr(1).split("&").forEach(function (item) {
-    request.GET[item.split("=")[0]] = item.split("=")[1].replace(/\+/g, ' ');
+    request.GET[item.split("=")[0]] = decodeURIComponent(item.split("=")[1].replace(/\+/g, ' '));
 });
