4c71e73ee663bacfb5965f8a2d06eed8ac19a17c
Dmitry Astrikov <astrikov.d@gmail.com>
2018-11-13 09:08:34 +0700

    Update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ packages = find_packages()
 
 setup(
     name='dartcms',
-    version='0.1.11',
+    version='0.1.12',
     description='DartCMS - open-source content management system for Django',
     long_description='DartCMS is an open-source content management system based on the popular '
                      'Django Framework. It is friendly for developers and end-users.',
