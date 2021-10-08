41af3ecfcff73d3fb6a483e3a6ca1c4acb6278fa
Slava M <master@neucom.ru>
2021-10-08 19:29:17 +0700

    Fix users migrations

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ packages = find_packages()
 
 setup(
     name='dartcms',
-    version='0.2.0',
+    version='0.2.1',
     description='DartCMS - open-source content management system for Django',
     long_description='DartCMS is an open-source content management system based on the popular '
                      'Django Framework. It is friendly for developers and end-users.',
